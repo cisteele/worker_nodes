c4808c40cad6260f8419baad87a5382143898fde
Christopher Ian Steele <57812260+cisteele@users.noreply.github.com>
2020-10-08 17:33:09 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+Directions:
+
+See README.md
+"""
+
+from api import DataApi, tests
+from statistics import mean, mode, median
+
+# Write your code here #
+
+def worker(whoami, api):
+  
+    pass
+
+
+# # # # # # # # # # # ##
+
+tests(worker, sum)
+
+
+# Uncomment below for extra credit!
+
+# tests(worker, mean)
+# tests(worker, mode)
+# tests(worker, median)
